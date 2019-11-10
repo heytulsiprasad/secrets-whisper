8519c9c571236cd8facc6f1af88ec783e91b6b9c
Tulsi Prasad <tulsi.prasad50@gmail.com>
2019-11-10 17:36:33 +0530

    password encryption

--- app.js
+++ app.js
@@ -3,12 +3,26 @@
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const userSchema = new mongoose.Schema ({
+    email: String,
+    password: String
+});
+
+const secret = "Thissecretisawesome";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", function (req, res) {
     res.render("home");
@@ -22,7 +36,39 @@ app.get("/register", function (req, res) {
     res.render("register");
 });
 
+// TODO: add users through registrations
+app.post("/register", function (req, res) {
+    const newUser = new User ({
+        email: req.body.username,
+        password: req.body.password
+    });
 
+    newUser.save(function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("secrets");
+        }
+    });
+});
+
+// TODO: add users through login:
+app.post("/login", function (req, res) {
+    username = req.body.username;
+    password = req.body.password;
+
+    User.findOne({email: username}, function (err, foundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                if (foundUser.password === password) {
+                    res.render("secrets");
+                }
+            }
+        }
+    });
+});
 
 app.listen(3000, function() {
     console.log("Server started at port 3000.");
